9a55caf206e6c2d5b0042db66e39150861f0ad0b
Abelradblanco <abelb200@gmail.com>
2024-05-10 22:38:45 -0600

    add update functionality

--- index.js
+++ index.js
@@ -36,9 +36,26 @@ app.post('/api/products', async (req, res) => {
         const product = await Product.create(req.body);
         res.status(200).json(product);
     }catch(error){
-        res.send(500).json({message: error.message});
+        res.status(500).json({message: error.message});
     }
 });
+
+//update
+app.put('/api/product/:id', async (req, res) => {
+    try{
+        const {id} = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message: "Product not found"});
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
+
+
 
 mongoose.connect("mongodb+srv://abelb200:Password@nodecrudapi.86khovx.mongodb.net/Node-Crud-API?retryWrites=true&w=majority&appName=NodeCrudAPI")
 .then(()=>{
